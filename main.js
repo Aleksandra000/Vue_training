b4abeb8d502ed3184f5866931e21eacd7aaf4957
Aleksandra000 <ao.dobrowolska@gmail.com>
2020-04-03 16:17:58 +0200

    first commit

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
-document.addEventListener("DOMContentLoaded", function(e) {
+document.addEventListener('DOMContentLoaded', function() {
 
+    const btns = document.querySelectorAll(".btn");
+    console.log(btns);
+    btns.forEach(btn => btn.addEventListener("click", finishedTask))
+
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(function (el) {
+        el.addEventListener('click', function (e) {
+            let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+            this.style.backgroundColor = randomColor;
+        })
+    });
 
 });
+
+function finishedTask(event) {
+    event.preventDefault();
+
+    console.log(this.dataset.id);
+    let li = document.querySelector(`li[data-id = "${this.dataset.id}"]`);
+    console.log(li);
+
+    li.classList.add("finished");
+
+    this.classList.remove("btn-primary");//lub event.target zamiast this
+    this.classList.add("btn-success");
+    this.innerText="Zakończone";
+    this.removeEventListener("click", finishedTask);
+    this.addEventListener("click", blockRefresh);
+
+}
+
+function blockRefresh(e) {
+    e.preventDefault();
+    console.log("xxx");//sprawia ze po drugim kliknięciu nie odklikują się wszystkie jak przedtem
+}
